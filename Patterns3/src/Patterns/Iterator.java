db3b988691d3bbb24c3cc70243ffceb3e3e2c1af
user-penguin <kobzev96@gmail.com>
2018-11-04 21:17:03 +0600

    initial

---
+++ Patterns3/src/Patterns/Iterator.java
@@ -0,0 +1,8 @@
+package Patterns;
+
+public interface Iterator {
+    Object first();
+    Object next();
+    boolean isDone();
+    Object currentItem();
+}
---
+++ Patterns3/src/Patterns/OrdinaryLikeCrazy.java
@@ -0,0 +1,3 @@
+package Patterns;
+
+public class OrdinaryLikeCrazy {}
