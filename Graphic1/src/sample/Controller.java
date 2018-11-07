009c36d8d7592f8a5d6471a76fcb58f5a71b7757
Kirill <kirsh.ru@yandex.ru>
2018-11-07 16:49:35 +0700

    Label display values add scaling and transfer

--- Graphic1/src/sample/Controller.java
+++ Graphic1/src/sample/Controller.java
@@ -28,7 +28,29 @@ public class Controller {
             lineD.setStartY(dots[i][1]);
             lineD.setEndX(dots[i + 1][0]);
             lineD.setEndY(dots[i + 1][1]);
-            lineD.setStrokeWidth(5);
+            lineD.setStrokeWidth(3);
+            lineD.setStroke(Color.PINK);
+            group.getChildren().addAll(lineD);
+        }
+
+        scene = new Scene(group, 800, 600, true);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void acceptXRot () {
+        stage = new Stage();
+        Group group = new Group();
+
+        double dots[][] = calculateOrto(calculateScale(Dots.dots, 20, 20, 10));
+
+        for (int i = 0; i < dots.length - 1; i++) {
+            Line lineD = new Line();
+            lineD.setStartX(dots[i][0]);
+            lineD.setStartY(dots[i][1]);
+            lineD.setEndX(dots[i + 1][0]);
+            lineD.setEndY(dots[i + 1][1]);
+            lineD.setStrokeWidth(3);
             lineD.setStroke(Color.PINK);
             group.getChildren().addAll(lineD);
         }
--- Graphic1/src/transform/TransformLib.java
+++ Graphic1/src/transform/TransformLib.java
@@ -21,4 +21,28 @@ public class TransformLib {
         }
         return dots;
     }
+    // повернуть x
+    public static double[][] calculateRotateX(double[][] stock, double angle) {
+        double[][] dots = new double[stock.length][];
+        for (int i = 0; i < stock.length; i++) {
+            dots[i] = Math.multiple(stock[i], DefaultTransform.getRotateMatrixX(angle));
+        }
+        return dots;
+    }
+    // повернуть y
+    public static double[][] calculateRotateY(double[][] stock, double angle) {
+        double[][] dots = new double[stock.length][];
+        for (int i = 0; i < stock.length; i++) {
+            dots[i] = Math.multiple(stock[i], DefaultTransform.getRotateMatrixY(angle));
+        }
+        return dots;
+    }
+    // повернуть z
+    public static double[][] calculateRotateZ(double[][] stock, double angle) {
+        double[][] dots = new double[stock.length][];
+        for (int i = 0; i < stock.length; i++) {
+            dots[i] = Math.multiple(stock[i], DefaultTransform.getRotateMatrixZ(angle));
+        }
+        return dots;
+    }
 }
