8967d56aa258eb712cc25e3be87e12284b8f720f
user-penguin <kobzev96@gmail.com>
2018-11-07 10:10:16 +0600

    govnocommit

--- Graphic1/src/compute/MathTest.java
+++ Graphic1/src/compute/MathTest.java
@@ -53,4 +53,24 @@ public class MathTest {
         double [][] expected = compute.Math.multiple(a, b);
         assertArrayEquals(actual, expected);
     }
+
+    @Test
+    public void multipleVectorOnMatrix() {
+        double [][] actual = {
+                {14, 32, 0}
+        };
+
+        double a[][] = {
+                {14, 32, 12}
+        };
+
+        double b[][] = {
+                {1, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}
+        };
+
+        double [][] expected = compute.Math.multiple(a, b);
+        assertArrayEquals(actual, expected);
+    }
 }
