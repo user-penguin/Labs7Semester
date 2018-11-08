b9961bee13258d5122141dce16af8b592d443786
user-penguin <kobzev96@gmail.com>
2018-11-08 12:04:22 +0600

    add scale by dot

--- Graphic1/src/transform/TransformLib.java
+++ Graphic1/src/transform/TransformLib.java
@@ -21,6 +21,28 @@ public class TransformLib {
         }
         return dots;
     }
+
+    // растянуть относительно точки x0,y0,z0
+    public static double[][] calculateScaleByDot (double[][] stock, double xS, double yS, double zS,
+                                                  double x0, double y0, double z0) {
+        // создадим массив с точками, где начало координат смещено на x0, y0, z0
+        double[][] dots = new double[stock.length][];
+        for (int i = 0; i < stock.length; i++) {
+            dots[i] = new double[]{stock[i][0] - x0, stock[i][1] - y0, stock[i][2] - z0, 1};
+        }
+
+        // растягиваем фигуру
+        dots = calculateScale(dots, xS, yS, zS);
+
+        //возвращаем координаты на место
+        for (int i = 0; i < stock.length; i++) {
+            dots[i][0] += x0;
+            dots[i][1] += y0;
+            dots[i][2] += z0;
+        }
+        return dots;
+    }
+
     // повернуть x
     public static double[][] calculateRotateX(double[][] stock, double angle) {
         double[][] dots = new double[stock.length][];
@@ -29,6 +51,7 @@ public class TransformLib {
         }
         return dots;
     }
+
     // повернуть y
     public static double[][] calculateRotateY(double[][] stock, double angle) {
         double[][] dots = new double[stock.length][];
@@ -37,6 +60,7 @@ public class TransformLib {
         }
         return dots;
     }
+
     // повернуть z
     public static double[][] calculateRotateZ(double[][] stock, double angle) {
         double[][] dots = new double[stock.length][];
@@ -45,6 +69,7 @@ public class TransformLib {
         }
         return dots;
     }
+
     // отобразить относительно XY
     public static double[][] calculateReflectionXY(double[][] stock) {
         double[][] dots = new double[stock.length][];
@@ -53,6 +78,7 @@ public class TransformLib {
         }
         return dots;
     }
+
     // отобразить относительно YZ
     public static double[][] calculateReflectionYZ(double[][] stock) {
         double[][] dots = new double[stock.length][];
@@ -61,6 +87,7 @@ public class TransformLib {
         }
         return dots;
     }
+
     // отобразить относительно ZX
     public static double[][] calculateReflectionZX(double[][] stock) {
         double[][] dots = new double[stock.length][];
@@ -69,6 +96,7 @@ public class TransformLib {
         }
         return dots;
     }
+
     // переместить
     public static double[][] calculateTransfer(double[][] stock, double xTr, double yTr, double zTr) {
         double[][] dots = new double[stock.length][];
